27488fafe1629578a2c93924ad36b6a6d83ca318
Nicolás Cruz <107238722+user14721068@users.noreply.github.com>
2025-04-28 17:22:22 -0600

    Update 7 Experiment Analysis.sql

--- 7 Experiment Analysis.sql
+++ 7 Experiment Analysis.sql
@@ -54,9 +54,11 @@ Para probar que A causa B, se necesitan:
 /* 7-4 Experimentos Binarios */
 
 /* Experimento binario: Buscamos ver si se realizó o no una acción de interés. Ejemplo: Los clientes dan click o no dan click en una página web.
-   Experimento contínuo: Estudiar una variable cuantitativa. Por ejemplo, cuanto dinero gastó un cliente en mi sitio web.*/
+   Experimento contínuo: Estudiar una variable cuantitativa. Por ejemplo, cuanto dinero gastó un cliente en mi sitio web.
+   Como herramientas estadísticas se usará la prueba ji-cuadrada y la prueba t de student
+   */
 
-/* Trabajaremos con datos de  */
+/* Trabajaremos con datos de una página web */
 select * 
 from experimentos.paginaweb;
 /*
@@ -68,4 +70,179 @@ user_id	variant	views	clicks	purchased_items	experiment_date	lastpurch_date	coun
 5	control	4	0	4	2020-01-01	2020-01-26	México
 ...
 */
+
+/* Agrupar por variable variant y obtener cuantos registros tienen click y cuantos no */
+select 	variant,
+		count(case when clicks=1 then 1 end) as si_click,
+        count(case when clicks=0 then 1 end) as no_click
+from experimentos.paginaweb
+group by 1;
+/*
+variant	si_click	no_click
+control	32628	32372
+test	32593	32407
+*/
+
+/* Agrupando por la variable variant obtener el total de usuario, el total de clicks y el porcentaje de clicks */
+select 	variant,
+		count(*) as intentos_totales,
+        count(case when clicks=1 then 1 end) as num_exitos,
+        count(case when clicks=1 then 1 end)/count(*) as porcentaje_exitos
+from experimentos.paginaweb
+group by 1;
+/*
+variant	intentos_totales	num_exitos	porcentaje_exitos
+control	65000	32628	0.5020
+test	65000	32593	0.5014
+*/
+
+/* Utilizando un software estadístico se puede realizar la prueba de hipotesis H0:La tasa de exito difiere entre ambos grupos vs H0:La tasa de exito no difiere */
+/* Con una confianza del 95% se rechaza H0 */
+
+
+/* 7-5 Experimentos Continuos */
+
+/* Queremos comproar si los valores promedio del experimento en el grupo de control y en el grupo con el "tratamiento" son significativamente diferentes 
+   La prueba que usaremos es la T de Student
+*/
+
+/* Obtener por cada grupo el numero de intentos totales, el promedio de visitas y la desviación estandar */
+select 	variant,
+		count(*) as tamaño_muestra,
+		avg(views) as promedio,
+        round(std(views),2) as desviacion_std
+from experimentos.paginaweb
+group by 1;
+/*
+variant	tamaño_muestra	promedio	desviacion_std
+control	65000	4.9802	5.83
+test	65000	5.0366	6.35
+*/
+
+/* Utilizando un software estadístico se puede realizar la prueba de hipotesis H0:el valor promedio entre ambos grupos es distinto vs H0:el valor promedio de mabos grupos no difiere */
+/* Con una confianza del 95% se rechaza H0 */
+
+/* 7-6 MAnejando valores sobresalientes */
+
+/* 
+Windsorizing: El proceso de reemplazar un número específico de valores extremos por valores más pequeños. 
+Binarizar: Tranformar la métrica de contínua a binaria.
+*/
+
+/* Agrupar por tipo de grupo, calcular cuantos usuarios hubo, cuantos clientes compraron, cuantos clientes no compraron */
+select 	variant,
+		count(distinct(user_id)) as usuarios_totales,
+		count(case when purchased_items > 0 then 1 end) as si_compro,
+		count(case when purchased_items = 0 then 1 end) as no_compro,
+        count(case when purchased_items > 0 then 1 end) / count(distinct(user_id)) as porcentaje_compra
+from experimentos.paginaweb
+group by 1;
+/*
+variant	usuarios_totales	si_compro	no_compro	porcentaje_compra
+control	65000	56934	8066	0.8759
+test	65000	56817	8183	0.8741
+*/
+
+/* Utilizando un software estadístico se puede realizar la prueba de hipotesis H0:El porcentaje de compras difiere entre ambos grupos vs H0:No difiere */
+/* Con una confianza del 95% se rechaza H0 */
+
+
+/* 7-7 Cajas de Tiempo */
+
+/* 
+Obtener la diferencia entre la fecha de observacion y fecha de ultima compra, el grupo, el usuario, el numero de articulos comprados, solo para
+aquellos registros donde la fecha de observacion y la fecha de ultima compra difieran a lo mas 15 días 
+*/
+select 	timestampdiff(day,experiment_date,lastpurch_date) as diferencia_dias,
+		variant,
+		user_id,
+        purchased_items
+from 	experimentos.paginaweb
+where 	timestampdiff(day,experiment_date,lastpurch_date) <= 15;
+/*
+diferencia_dias	variant	user_id	purchased_items
+9	control	1	4
+15	control	6	1
+0	control	8	1
+7	control	12	1
+3	control	13	2
+...
+*/
+
+/* Usando la consulta anterior obtener por tipo de grupo (control y tratamiento) el total de observaciones, el promedio de articulos comprados y
+la desviacion estandar del numero de articulos comprados. Usar la prueba t de student para probar la hipotesis de que la media
+de los articulos comprados difiere entre ambos grupos. 
+*/
+select 	variant, 
+		count(user_id) as num_observaciones,
+		avg(purchased_items) as prom_articulos,
+        round(std(purchased_items),2) as desv_est_articulos
+from
+(
+select 	timestampdiff(day,experiment_date,lastpurch_date) as diferencia_dias,
+		variant,
+		user_id,
+        purchased_items
+from 	experimentos.paginaweb
+where 	timestampdiff(day,experiment_date,lastpurch_date) <= 15
+) as a
+group by 1;
+/*
+variant	num_observaciones	prom_articulos	desv_est_articulos
+control	33566	3.4945	2.29
+test	33514	3.4970	2.30
+*/
+
+/* Utilizando un software estadístico se puede realizar la prueba de hipotesis H0:el numero de items promedio entre ambos grupos es distinto 
+vs H0:no es distinto. Conclusión: con una confianza del 95% se rechaza H0 */
+
+/* 7-8 Pre-Post */
+
+/*
+Análisis Pre-Post: Compara a la misma población antes y después de un cambio
+*/
+
+/* Realizar un analisis del comportamiento de las compras en la pagina web 5 días antes y 5 días después de día de reyes. Usar una prueba estadistica para
+probar la hipotesis de que la proporcion de compras es la misma en ambos grupos. */
+select 	case
+			when experiment_date >= "2020-01-01" and experiment_date <= "2020-01-05" then "pre"
+            when experiment_date >= "2020-01-06" and experiment_date <= "2020-01-10" then "post"
+		end as grupo,
+        count(user_id) as num_usuario,
+        count(case when purchased_items > 0 then 1 end) as compras,
+        count(case when purchased_items > 0 then 1 end) / count(user_id) as pct_compras
+from 	experimentos.paginaweb
+where 	experiment_date >= "2020-01-01" and experiment_date <= "2020-01-10"
+group by 1;
+/*
+grupo	num_usuario	compras	pct_compras
+pre	13560	11894	0.8771
+post	32428	28363	0.8746
+*/
+
+/* Utilizando un software estadístico se puede realizar la prueba de hipotesis H0:El porcentaje de compras difiere entre ambos grupos vs H0:No difiere */
+/* Con una confianza del 95% se rechaza H0 */
+
+/* 7-9-Analisis Experimento Natural */
+
+/* Experimento natural: Generalmente por razones no intencionales 2 poblaciones son separadas y se observa el comportamiento de ciertas variables en ambas poblaciones. */
+
+/* Comparar las ventas en Chile y Argentina y usar una prueba estadistica para probar la hipotesis de que el porcentaje de compras es distinto en ambos grupos */
+
+select 	country,
+		count(user_id) as usuarios,
+        count(case when purchased_items > 0 then 1 end) as compras,
+		count(case when purchased_items > 0 then 1 end) / count(user_id) as pct_compras
+from experimentos.paginaweb
+where country in ("Chile","Argentina")
+group by 1;
+/*
+country	usuarios	compras	pct_compras
+Argentina	22873	22866	0.9997
+Chile	26804	22802	0.8507
+*/
+
+/* Utilizando un software estadístico se puede realizar la prueba de hipotesis H0:El porcentaje de compras difiere entre ambos grupos vs H0:No difiere */
+/* Con una confianza del 95% no se rechaza H0 */
+
 
